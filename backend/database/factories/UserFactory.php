8e4e2022310652b38c34ba881773561d74357939
MohamedHamouch <med10hamouch@gmail.com>
2026-08-19 20:18:54 +0100

    feat: add admin, client, and worker factory states to UserFactory

--- backend/database/factories/UserFactory.php
+++ backend/database/factories/UserFactory.php
@@ -30,6 +30,7 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'role' => 'client',
         ];
     }
 
@@ -40,6 +41,27 @@ class UserFactory extends Factory
     {
         return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
+        ]);
+    }
+
+    public function admin(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'role' => 'admin',
+        ]);
+    }
+
+    public function client(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'role' => 'client',
+        ]);
+    }
+
+    public function worker(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'role' => 'worker',
         ]);
     }
 }
