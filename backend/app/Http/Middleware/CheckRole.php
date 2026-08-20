6ae38bd5b4539359aaa07c11c0eeabefc1b9edfd
MohamedHamouch <med10hamouch@gmail.com>
2026-08-20 01:07:22 +0100

    feat: add CheckRole middleware for route-level role authorization

---
+++ backend/app/Http/Middleware/CheckRole.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckRole
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @param  string  ...$roles
+     */
+    public function handle(Request $request, Closure $next, string ...$roles): Response
+    {
+        $user = $request->user();
+
+        if (! $user || ! in_array($user->role, $roles, true)) {
+            return response()->json([
+                'message' => 'Access denied. You do not have permission to access this resource.',
+            ], Response::HTTP_FORBIDDEN);
+        }
+
+        return $next($request);
+    }
+}
